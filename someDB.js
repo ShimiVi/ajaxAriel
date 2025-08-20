478eb88252117e51f768cb6ecbd2b090126b0925
Shimshon Vikel <Shimshon.Vikel@aman.co.il>
2025-08-20 11:48:18 +0300

    all

---
+++ someDB.js
@@ -0,0 +1,8 @@
+let someDb = {
+    "students": [
+        {name: "arial", age: 35}, 
+        {name: "arial 2", age: 22}, 
+        {name: "arial 4", age: 44}, 
+        {name: "arial 6", age: 126}, 
+    ]
+}
